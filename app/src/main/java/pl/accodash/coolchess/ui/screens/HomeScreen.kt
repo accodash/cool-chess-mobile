128ac34109704b526d4e62fb33ff4ec3053af109
palinkiewicz <pogromca.ap@gmail.com>
2025-05-19 14:44:17 +0200

    Remove unused imports

--- app/src/main/java/pl/accodash/coolchess/ui/screens/HomeScreen.kt
+++ app/src/main/java/pl/accodash/coolchess/ui/screens/HomeScreen.kt
@@ -1,12 +1,6 @@
 package pl.accodash.coolchess.ui.screens
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import pl.accodash.coolchess.R
 import pl.accodash.coolchess.api.models.User
 
 @Composable
